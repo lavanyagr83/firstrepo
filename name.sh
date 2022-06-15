127bd0fffe001b6bbdb5add82fa88e5c7f76500e
lavanyagr83 <plavanyag@gmail.com>
2022-06-15 14:08:17 +0530

    third commit

---
+++ name.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "enter your first name"
+read f
+if [[ "${f}" =~ [^a-zA-Z] ]]
+then
+echo invalid
+else
+echo valid
+fi
+echo "enter your last name"
+read l
+if [[ "${l}" =~ [^a-zA-Z] ]]
+then
+echo invalid
+else
+echo valid
+echo "${F}.${L}"
+echo -n "${F}${L}"|sha1sum
+fi
